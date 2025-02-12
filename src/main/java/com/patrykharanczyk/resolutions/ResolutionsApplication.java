514c30bac6d23a822a44866c7d6900782475b307
patryk.haranczyk <patryk023@gmail.com>
2025-02-12 22:04:19 +0100

    Initial commit

---
+++ src/main/java/com/patrykharanczyk/resolutions/ResolutionsApplication.java
@@ -0,0 +1,13 @@
+package com.patrykharanczyk.resolutions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResolutionsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResolutionsApplication.class, args);
+    }
+
+}
