e3c04b77a2e393331027e6376a52cb3156521aa2
patryk.haranczyk <patryk023@gmail.com>
2025-02-17 22:49:38 +0100

    Removed unnecessary import

--- src/main/java/com/patrykharanczyk/resolutions/repository/ResolutionRepository.java
+++ src/main/java/com/patrykharanczyk/resolutions/repository/ResolutionRepository.java
@@ -3,8 +3,6 @@ package com.patrykharanczyk.resolutions.repository;
 import com.patrykharanczyk.resolutions.model.Resolution;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface ResolutionRepository extends JpaRepository<Resolution, Long> {
 
 }
